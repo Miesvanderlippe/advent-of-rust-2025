4ee0a1d34bbbbb181b8e5100e89a65d535c47284
Mies van der Lippe <miesvanderlippe@gmail.com>
2025-12-05 13:12:59 +0100

    Day05 - Part2

--- day05/src/bin/day05.rs
+++ day05/src/bin/day05.rs
@@ -1,7 +1,7 @@
 use std::{fs, path::PathBuf};
 
 use clap::Parser;
-use day05::{parse_input, solve_part_1};
+use day05::{parse_input, solve_part_1, solve_part_2};
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -16,8 +16,10 @@ fn main() {
             let (_, (fresh_ranges, ingredients)) =
                 parse_input(&text).expect("Failed to parse the input.");
             let part_1_ans = solve_part_1(&fresh_ranges, &ingredients);
+            let part_2_ans = solve_part_2(&fresh_ranges);
 
-            println!("Fresh ingredients: {}", part_1_ans);
+            println!("Fresh ingredients in inventory: {}", part_1_ans);
+            println!("All fresh ingredients: {}", part_2_ans);
         }
         Err(file_error) => println!("Failed to open puzzle input with error {}", file_error),
     }
--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -32,6 +32,54 @@ pub fn solve_part_1(fresh_ranges: &Vec<(usize, usize)>, ingredients: &Vec<usize>
         .count()
 }
 
+pub fn solve_part_2(fresh_ranges: &Vec<(usize, usize)>) -> usize {
+    let mut condensed_ranges: Vec<(usize, usize)> = vec![];
+
+    for (fr_start, fr_end) in fresh_ranges {
+        let overlapping_condensed_ranges: Vec<(usize, (usize, usize))> = condensed_ranges
+            .iter()
+            .enumerate()
+            .filter(|(_, (cr_start, cr_end))| {
+                // The start or end of fresh range is within an existing cr_range.
+                (
+                    // Start
+                    (fr_start >= cr_start && fr_start <= cr_end)
+                    ||
+                    // End
+                    (fr_end >= cr_start && fr_end <= cr_end)
+                )
+                // The fresh range overlaps the entire condensed range
+                || (fr_start <= cr_start && fr_end >= cr_end)
+            })
+            .map(|(index, (s, e))| (index, (*s, *e)))
+            .collect();
+
+        let mut new_start = *fr_start;
+        let mut new_end = *fr_end;
+        let mut delete_indexes = vec![];
+
+        for (old_index, (cr_start, cr_end)) in overlapping_condensed_ranges {
+            delete_indexes.push(old_index);
+            if cr_start < new_start {
+                new_start = cr_start
+            }
+            if cr_end > new_end {
+                new_end = cr_end
+            }
+        }
+        delete_indexes.sort();
+        delete_indexes.reverse();
+        for di in delete_indexes {
+            condensed_ranges.remove(di);
+        }
+        condensed_ranges.push((new_start, new_end));
+    }
+
+    condensed_ranges
+        .iter()
+        .fold(0, |acc, (cr_start, cr_end)| acc + (cr_end - cr_start) + 1)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -82,5 +130,56 @@ mod tests {
         let (_, (fresh_ranges, ingredients)) =
             parse_input(example_input).expect("Changed the parser?");
         assert_eq!(solve_part_1(&fresh_ranges, &ingredients), 3)
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let example_input = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32";
+        let (_, (fresh_ranges, _)) = parse_input(example_input).expect("Changed the parser?");
+        assert_eq!(solve_part_2(&fresh_ranges), 14)
+    }
+
+    #[test]
+    fn test_part2_folding_1() {
+        let example_input = "1-10
+15-20
+9-16
+
+1";
+        let (_, (fresh_ranges, _)) = parse_input(example_input).expect("Changed the parser?");
+        assert_eq!(solve_part_2(&fresh_ranges), 20)
+    }
+
+    #[test]
+    fn test_part2_folding_2() {
+        let example_input = "1-10
+9-12
+12-13
+
+1";
+        let (_, (fresh_ranges, _)) = parse_input(example_input).expect("Changed the parser?");
+        assert_eq!(solve_part_2(&fresh_ranges), 13)
+    }
+
+    #[test]
+    fn test_part2_folding_3() {
+        let example_input = "3-6
+8-11
+13-15
+1-20
+
+1";
+        let (_, (fresh_ranges, _)) = parse_input(example_input).expect("Changed the parser?");
+        assert_eq!(solve_part_2(&fresh_ranges), 20)
     }
 }
