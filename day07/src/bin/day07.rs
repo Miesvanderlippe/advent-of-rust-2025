d43a8d4f5eedf1b4bee7fedd233070712aa0e233
Mies van der Lippe <miesvanderlippe@gmail.com>
2025-12-07 09:46:22 +0100

    Day07 - Part2

--- day07/src/bin/day07.rs
+++ day07/src/bin/day07.rs
@@ -1,7 +1,7 @@
 use std::{fs, path::PathBuf};
 
 use clap::Parser;
-use day07::solve_part_1;
+use day07::{solve_part_1, solve_part_2};
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -14,8 +14,10 @@ fn main() {
     match fs::read_to_string(input.puzzle_input) {
         Ok(text) => {
             let part_1_ans = solve_part_1(&text).expect("Expected valid puzzle input.");
+            let part_2_ans = solve_part_2(&text).expect("Expected valid puzzle input.");
 
             println!("Splits in part 1: {}", part_1_ans);
+            println!("Splits in part 2: {}", part_2_ans);
         }
         Err(file_error) => println!("Failed to open puzzle input with error {}", file_error),
     }
--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -26,6 +26,45 @@ pub fn solve_part_1(puzzle_input: &str) -> Result<usize, ()> {
     Ok(splits)
 }
 
+//           S
+//           |               1
+//           ^
+//          | |              2
+//          ^ ^
+//         | | |             4 (routes leading to centre: 2, routes leading to edges: 1)
+//         ^ | |
+//        | || |
+//
+//
+//
+
+pub fn solve_part_2(puzzle_input: &str) -> Result<usize, ()> {
+    let lines: Vec<&[u8]> = puzzle_input.lines().map(|l| l.as_bytes()).collect();
+    let width = lines[0].len();
+
+    let mut routes_leading_here: Vec<usize> = lines[0]
+        .iter()
+        .map(|b| if *b == b'S' { 1 } else { 0 })
+        .collect();
+
+    for row in 1..lines.len() {
+        let mut next_routes_leading_here = vec![0_usize; width];
+
+        for col in 0..width {
+            if routes_leading_here[col] != 0 && lines[row][col] == b'^' {
+                next_routes_leading_here[col - 1] += routes_leading_here[col];
+                next_routes_leading_here[col + 1] += routes_leading_here[col];
+            } else if routes_leading_here[col] != 0 && lines[row][col] == b'.' {
+                next_routes_leading_here[col] += routes_leading_here[col];
+            }
+        }
+
+        routes_leading_here = next_routes_leading_here;
+    }
+
+    Ok(routes_leading_here.iter().sum())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,5 +88,26 @@ mod tests {
 .^.^.^.^.^...^.
 ...............";
         assert_eq!(solve_part_1(example_input), Ok(21));
+    }
+
+    #[test]
+    fn test_example_part2() {
+        let example_input = ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
+        assert_eq!(solve_part_2(example_input), Ok(40));
     }
 }
